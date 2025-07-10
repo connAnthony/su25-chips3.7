e4b08154aaa90f335cd48b06cd76ddd8947d7fec
Connor Fagan <connorfagan@berkeley.edu>
2025-07-09 21:23:25 -0700

    Logic done

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -6,6 +6,50 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+
+  def guess(char)
+    if char.nil? || char.empty? || !char.match("[A-Za-z]")
+      raise ArgumentError, "Enter a single letter"
+    end
+    char = char.downcase
+    if !@guesses.include?(char) && @word.include?(char)
+      @guesses += char
+    elsif !@word.include?(char) && !@wrong_guesses.include?(char)
+      @wrong_guesses += char
+    else
+      false
+    end
+  end
+
+  def word_with_guesses
+    builder = ""
+    @word.each_char do |char|
+      if @guesses.include?(char)
+        builder += char
+      else
+        builder += "-"
+      end
+    end
+    builder
+  end
+
+  def check_win_or_lose
+    if @wrong_guesses.length >= 7
+        return :lose
+    end
+    @word.each_char do |char|
+      if !(@guesses.include?(char))
+        return :play
+      end
+    end
+    :win
   end
 
   # You can test it by installing irb via $ gem install irb
